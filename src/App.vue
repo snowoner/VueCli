ee6c325363e8669cbd4c82688c9396f2985e4d2e
oscar urgelles <oscar.urgelles@gmail.com>
2019-04-29 15:41:00 +0200

    ASD

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,58 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Pokemons on VUE CLI"/>
-    <MyTitle title="Hola"/>
+    <h1>My Bookstore</h1>
+    <input type="text" placeholder="Search a Book.." id="search" v-model="searchValue"/>
+    <div v-for="(book,index) in books" :key="index">{{book.titulo}}</div>
+    <p>Hola</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import MyTitle from './components/MyTitle.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import MyTitle from './components/MyTitle.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      url:"https://api.myjson.com/bins/p803g",
+      books:[],
+      searchValue:""
+    }
+  },
+  methods:{
+    getData(){
+      fetch(this.url)
+      .then(data=>data.json())
+      .then(data=>{
+        this.books=data.books;
+      })
+      .catch(error => alert(error));
+    },
+  },
+  computed: {
+    printbooks(){
+      if(this.searchValue){
+        return this.books.filter(book=>{
+          return book.titulo.includes(this.searchValue) || book.descripcion.includes(this.searchValue);
+         });
+      }
+      else{
+        return this.books;
+      }
+    }
+  },
+  created() {
+    this.getData();
+  },
   components: {
-    HelloWorld,
-    MyTitle
+    // HelloWorld,
+    // MyTitle
   }
 }
 </script>
 
 <style>
+
 
 </style>
